6fcf58fb393e46cfee05fbc6a4aa456c08bf7a6d
Vishalkumar-Prajapati <vishalkumar.prajapati@yudiz.com>
2023-04-18 11:39:55 +0530

    crypto chipher iv

---
+++ cryptoUsingChipher.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const crypto = require('crypto')
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    let data = '';
+    req.on('data', (chunk) => {
+        data += chunk;
+    });
+    req.on('end', () => {
+        const parsed = JSON.parse(data);
+        let { username, password } = parsed;
+        function encrypt(text) {
+            const chipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+            let encrypted = chipher.update('chipher', 'utf-8', 'hex');
+            encrypted += chipher.final('hex')
+            return encrypted;
+        }
+        const algorithm = 'aes-256-cbc';      //algoritham for encrypt
+        const key = crypto.randomBytes(32);   //key for encrypt
+        const iv = crypto.randomBytes(16);    //iv for encrypt
+        let encrypted = encrypt(password);    //call function encrypt 
+        let oData = { username: username, password: encrypted };
+        const oReadData = JSON.parse(fs.readFileSync('./Crypto/database.json', 'utf-8'));
+        oReadData.aData.push(oData); //we have a aData arr in oReadData object form database.json
+        let stringify = JSON.stringify(oReadData); 
+        fs.writeFile('./Crypto/database.json', stringify, function (err) {
+            if (err) console.log(err);
+            console.log('seved');
+        });
+        return res.end(stringify);
+    });
+});
+server.listen(8080, (err) => {
+    if (err) console.log(err);
+    else {
+        console.log('listening on port no 8080');
+    }
+});
